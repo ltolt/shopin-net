809c9d29e422107cc85fc0f87a2fd08d508e337e
lucas421634258@gmail.com <lucas421634258@gmail.com@3b30caeb-a79e-e7b5-a4dc-2553bdc3c635>
2013-09-10 12:47:05 +0000

    代码svn提交时,使用注释模板 svn制作模板过程 svn --> 提交 --> Configure Comment Template .. --> 注释模板 -->新建 Commend By  [luxiangxing 姓名] : [NETCURRENT-2 --问题号]  #resolve #comment [注释] 1. 2.

--- trunk/framework/gson/guide/GsonStart.java
+++ trunk/framework/gson/guide/GsonStart.java
@@ -15,6 +15,7 @@ import javax.swing.event.RowSorterEvent.Type;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
